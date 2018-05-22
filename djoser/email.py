1ab294c212fb38dacb0860c5f348150fec98ec41
Oliver Zhou <oliver@hirevisor.com>
2018-05-22 11:08:14 -0700

    JWT tokens being passed thru to automatically log you in after clicking the activation URL

--- djoser/email.py
+++ djoser/email.py
@@ -4,6 +4,7 @@ from templated_mail.mail import BaseEmailMessage
 
 from djoser import utils
 from djoser.conf import settings
+from rest_framework_jwt.settings import api_settings
 
 
 class ActivationEmail(BaseEmailMessage):
@@ -15,6 +16,11 @@ class ActivationEmail(BaseEmailMessage):
         user = context.get('user')
         context['uid'] = utils.encode_uid(user.pk)
         context['token'] = default_token_generator.make_token(user)
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        payload = jwt_payload_handler(user)
+        # TODO - migrate off needing this in activation email
+        context['jwt_token'] = jwt_encode_handler(payload)
         context['url'] = settings.ACTIVATION_URL.format(**context)
         return context
 
